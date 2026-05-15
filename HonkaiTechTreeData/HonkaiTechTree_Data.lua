a272fd6378fd11596311a5e96a867af0a50d36a8
Albert14736 <am5225@ic.ac.uk>
2026-05-15 12:48:34 +0100

    9

--- HonkaiTechTreeData/HonkaiTechTree_Data.lua
+++ HonkaiTechTreeData/HonkaiTechTree_Data.lua
@@ -207,6 +207,188 @@ function GetHonkaiTechTreeData()
 			UITreeRow = 3,
 			Prereqs = {"HONKAI_TECH_WEAPON_PROTOTYPE", "HONKAI_TECH_ATONEMENT_SYSTEM", "HONKAI_TECH_KNIGHT_LEGACY"},
 			Column = 3
+		},
+		-- ==============================================
+		-- 中世纪 (ERA_MEDIEVAL)
+		-- ==============================================
+		-- 第 1 列 (Cost: 275)
+		{
+			Type = "HONKAI_TECH_STIGMATA_PRINCIPLE",
+			Name = "LOC_HONKAI_TECH_STIGMATA_PRINCIPLE_NAME",
+			Description = "LOC_HONKAI_TECH_STIGMATA_PRINCIPLE_DESC",
+			Cost = 275,
+			EraType = "ERA_MEDIEVAL",
+			UITreeRow = -3,
+			Prereqs = {"HONKAI_TECH_POWER_ARMOR"},
+			Column = 1
+		},
+		{
+			Type = "HONKAI_TECH_GREAT_ERUPTION_HYPOTHESIS",
+			Name = "LOC_HONKAI_TECH_GREAT_ERUPTION_HYPOTHESIS_NAME",
+			Description = "LOC_HONKAI_TECH_GREAT_ERUPTION_HYPOTHESIS_DESC",
+			Cost = 275,
+			EraType = "ERA_MEDIEVAL",
+			UITreeRow = -1,
+			Prereqs = {"HONKAI_TECH_POWER_ARMOR"},
+			Column = 1
+		},
+		{
+			Type = "HONKAI_TECH_VALKYRIE_BLOODLINE_PURIFICATION",
+			Name = "LOC_HONKAI_TECH_VALKYRIE_BLOODLINE_PURIFICATION_NAME",
+			Description = "LOC_HONKAI_TECH_VALKYRIE_BLOODLINE_PURIFICATION_DESC",
+			Cost = 275,
+			EraType = "ERA_MEDIEVAL",
+			UITreeRow = 2,
+			Prereqs = {"HONKAI_TECH_VALKYRIE_ADVANCED"},
+			Column = 1
+		},
+		{
+			Type = "HONKAI_TECH_CLERGY_PRIVILEGE",
+			Name = "LOC_HONKAI_TECH_CLERGY_PRIVILEGE_NAME",
+			Description = "LOC_HONKAI_TECH_CLERGY_PRIVILEGE_DESC",
+			Cost = 275,
+			EraType = "ERA_MEDIEVAL",
+			UITreeRow = 4,
+			Prereqs = {"HONKAI_TECH_VALKYRIE_ADVANCED"},
+			Column = 1
+		},
+		-- 第 2 列 (Cost: 300)
+		{
+			Type = "HONKAI_TECH_SOULLIQUOR_SMELTING",
+			Name = "LOC_HONKAI_TECH_SOULLIQUOR_SMELTING_NAME",
+			Description = "LOC_HONKAI_TECH_SOULLIQUOR_SMELTING_DESC",
+			Cost = 300,
+			EraType = "ERA_MEDIEVAL",
+			UITreeRow = -2,
+			Prereqs = {"HONKAI_TECH_STIGMATA_PRINCIPLE", "HONKAI_TECH_GREAT_ERUPTION_HYPOTHESIS"},
+			Column = 2
+		},
+		{
+			Type = "HONKAI_TECH_SCHICKSAL_ARMED_FORMATION",
+			Name = "LOC_HONKAI_TECH_SCHICKSAL_ARMED_FORMATION_NAME",
+			Description = "LOC_HONKAI_TECH_SCHICKSAL_ARMED_FORMATION_DESC",
+			Cost = 300,
+			EraType = "ERA_MEDIEVAL",
+			UITreeRow = 3,
+			Prereqs = {"HONKAI_TECH_VALKYRIE_BLOODLINE_PURIFICATION", "HONKAI_TECH_CLERGY_PRIVILEGE"},
+			Column = 2
+		},
+		-- 第 3 列 (Cost: 390)
+		{
+			Type = "HONKAI_TECH_HONKAI_CRYSTAL_EXTRACTION",
+			Name = "LOC_HONKAI_TECH_HONKAI_CRYSTAL_EXTRACTION_NAME",
+			Description = "LOC_HONKAI_TECH_HONKAI_CRYSTAL_EXTRACTION_DESC",
+			Cost = 390,
+			EraType = "ERA_MEDIEVAL",
+			UITreeRow = -1,
+			Prereqs = {"HONKAI_TECH_SOULLIQUOR_SMELTING"},
+			Column = 3
+		},
+		{
+			Type = "HONKAI_TECH_GOD_SLAYER_ARMOR_PROTOTYPE",
+			Name = "LOC_HONKAI_TECH_GOD_SLAYER_ARMOR_PROTOTYPE_NAME",
+			Description = "LOC_HONKAI_TECH_GOD_SLAYER_ARMOR_PROTOTYPE_DESC",
+			Cost = 390,
+			EraType = "ERA_MEDIEVAL",
+			UITreeRow = 2,
+			Prereqs = {"HONKAI_TECH_SCHICKSAL_ARMED_FORMATION"},
+			Column = 3
+		},
+		-- ==============================================
+		-- 文艺复兴时代 (ERA_RENAISSANCE)
+		-- ==============================================
+		-- 第 1 列 (Cost: 490)
+		{
+			Type = "HONKAI_TECH_IMAGINARY_INTERFERENCE_THEORY",
+			Name = "LOC_HONKAI_TECH_IMAGINARY_INTERFERENCE_THEORY_NAME",
+			Description = "LOC_HONKAI_TECH_IMAGINARY_INTERFERENCE_THEORY_DESC",
+			Cost = 490,
+			EraType = "ERA_RENAISSANCE",
+			UITreeRow = -3,
+			Prereqs = {"HONKAI_TECH_HONKAI_CRYSTAL_EXTRACTION"},
+			Column = 1
+		},
+		{
+			Type = "HONKAI_TECH_FISSION_REACTOR",
+			Name = "LOC_HONKAI_TECH_FISSION_REACTOR_NAME",
+			Description = "LOC_HONKAI_TECH_FISSION_REACTOR_DESC",
+			Cost = 490,
+			EraType = "ERA_RENAISSANCE",
+			UITreeRow = -1,
+			Prereqs = {"HONKAI_TECH_HONKAI_CRYSTAL_EXTRACTION"},
+			Column = 1
+		},
+		{
+			Type = "HONKAI_TECH_GRAY_SERPENT_NETWORK",
+			Name = "LOC_HONKAI_TECH_GRAY_SERPENT_NETWORK_NAME",
+			Description = "LOC_HONKAI_TECH_GRAY_SERPENT_NETWORK_DESC",
+			Cost = 490,
+			EraType = "ERA_RENAISSANCE",
+			UITreeRow = 0,
+			Prereqs = {"HONKAI_TECH_HONKAI_CRYSTAL_EXTRACTION"},
+			Column = 1
+		},
+		{
+			Type = "HONKAI_TECH_ABSOLUTE_THEOCRACY",
+			Name = "LOC_HONKAI_TECH_ABSOLUTE_THEOCRACY_NAME",
+			Description = "LOC_HONKAI_TECH_ABSOLUTE_THEOCRACY_DESC",
+			Cost = 490,
+			EraType = "ERA_RENAISSANCE",
+			UITreeRow = 3,
+			Prereqs = {"HONKAI_TECH_GOD_SLAYER_ARMOR_PROTOTYPE"},
+			Column = 1
+		},
+		-- 第 2 列 (Cost: 540)
+		{
+			Type = "HONKAI_TECH_STIGMATA_GENE_COMPLETION",
+			Name = "LOC_HONKAI_TECH_STIGMATA_GENE_COMPLETION_NAME",
+			Description = "LOC_HONKAI_TECH_STIGMATA_GENE_COMPLETION_DESC",
+			Cost = 540,
+			EraType = "ERA_RENAISSANCE",
+			UITreeRow = -2,
+			Prereqs = {"HONKAI_TECH_IMAGINARY_INTERFERENCE_THEORY", "HONKAI_TECH_FISSION_REACTOR"},
+			Column = 2
+		},
+		{
+			Type = "HONKAI_TECH_FANG_IN_THE_SHADOWS",
+			Name = "LOC_HONKAI_TECH_FANG_IN_THE_SHADOWS_NAME",
+			Description = "LOC_HONKAI_TECH_FANG_IN_THE_SHADOWS_DESC",
+			Cost = 540,
+			EraType = "ERA_RENAISSANCE",
+			UITreeRow = 1,
+			Prereqs = {"HONKAI_TECH_GRAY_SERPENT_NETWORK"},
+			Column = 2
+		},
+		{
+			Type = "HONKAI_TECH_GOD_SLAYER_ARMOR_DEPLOYMENT",
+			Name = "LOC_HONKAI_TECH_GOD_SLAYER_ARMOR_DEPLOYMENT_NAME",
+			Description = "LOC_HONKAI_TECH_GOD_SLAYER_ARMOR_DEPLOYMENT_DESC",
+			Cost = 540,
+			EraType = "ERA_RENAISSANCE",
+			UITreeRow = 2,
+			Prereqs = {"HONKAI_TECH_ABSOLUTE_THEOCRACY"},
+			Column = 2
+		},
+		-- 第 3 列 (Cost: 600)
+		{
+			Type = "HONKAI_TECH_HONKAI_ISOLATION_DOME",
+			Name = "LOC_HONKAI_TECH_HONKAI_ISOLATION_DOME_NAME",
+			Description = "LOC_HONKAI_TECH_HONKAI_ISOLATION_DOME_DESC",
+			Cost = 600,
+			EraType = "ERA_RENAISSANCE",
+			UITreeRow = -1,
+			Prereqs = {"HONKAI_TECH_STIGMATA_GENE_COMPLETION"},
+			Column = 3
+		},
+		{
+			Type = "HONKAI_TECH_FLOATING_ISLAND_TECH",
+			Name = "LOC_HONKAI_TECH_FLOATING_ISLAND_TECH_NAME",
+			Description = "LOC_HONKAI_TECH_FLOATING_ISLAND_TECH_DESC",
+			Cost = 600,
+			EraType = "ERA_RENAISSANCE",
+			UITreeRow = 3,
+			Prereqs = {"HONKAI_TECH_GOD_SLAYER_ARMOR_DEPLOYMENT"},
+			Column = 3
 		}
 	}
 end
